35edc339ab3f2aa5f31b234ca747aa9058faab35
Max <xiehongfeng100@qq.com>
2015-05-05 23:23:16 +0800

    Update BFS_n_DFS.h

--- Graph/Graph/BFS_n_DFS.h
+++ Graph/Graph/BFS_n_DFS.h
@@ -9,17 +9,17 @@ const int MAXNUM = 1024;
 class ENode
 {
 public:
-	int firstVex;				// the first vertex on the edge
-	ENode * nextEdge;			// point to the next edge
+	int firstVex;						// the first vertex on the edge
+	ENode * nextEdge;					// point to the next edge
 };
 
 class VNode
 {
 public:
-	int id;						// id of the vertex
-	int distance;				// distance = -1 means infinite
-	bool isVisited;				// indicate whether the vertex has been visited or not
-	ENode * firstEdge;			// point to its first edge
+	int id;							// id of the vertex
+	int distance;						// distance = -1 means infinite
+	bool isVisited;						// indicate whether the vertex has been visited or not
+	ENode * firstEdge;					// point to its first edge
 };
 
 class Graph
@@ -29,15 +29,15 @@ public:
 	virtual ~Graph();
 	void init(int * firstVex, int * secondVex, int len);	// initialization of the whole graph
 
-	void BFS();							// breadth first search
-	void DFS();							// depth first search
-	void BFS(int vexID);				// breadth first search
-	void DFS(int vexID);				// depth first search
-	vector<int> pathTo(int vexID);		// return path to the vex
+	void BFS();						// breadth first search
+	void DFS();						// depth first search
+	void BFS(int vexID);					// breadth first search
+	void DFS(int vexID);					// depth first search
+	vector<int> pathTo(int vexID);				// return path to the vex
 
-	vector<int> adj(int vexID);			// return the vertexes next to the vex
-	int V();							// return the number of vertexes
-	int E();							// return the number of edges
+	vector<int> adj(int vexID);				// return the vertexes next to the vex
+	int V();						// return the number of vertexes
+	int E();						// return the number of edges
 	//void print();						// 
 
 	// just for test
@@ -46,9 +46,9 @@ public:
 
 private:
 	bool addEdge(int firstVex, int secondVex);
-	VNode vexNode[MAXNUM];				// vertexes
+	VNode vexNode[MAXNUM];					// vertexes
 	int edgeTo[MAXNUM];					// a vector of available edge between two vertexes
-	int vexNum;							// number of vertexes
+	int vexNum;						// number of vertexes
 	int edgeNum;						// number of edges
 
 	// just for test
